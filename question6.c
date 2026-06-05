a927206171747d8127cff5246eb983c28dcd8f30
Anvesha132 <anveshak919@gmail.com>
2026-06-05 08:28:43 +0530

    calculate area and perimeter of rectangle

---
+++ question6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int h=7,w=5;
+    int area,peri;
+    area=(h*w);
+    peri=2*(h+w);
+    printf("Area=%d\n",area);
+    printf("perimeter=%d\n",peri);
+    return 0;
+}
